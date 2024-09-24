2ae7a0bf3ce2368d9d8ef56b4ebe065b5891c449
Harrison Hank <codinghank@gmail.com>
2024-09-24 21:14:31 +0300

    added schemas for documentation

--- savannah/api/utils.py
+++ savannah/api/utils.py
@@ -1,22 +1,3 @@
-# import africastalking
-# from django.conf import settings
-# import re
-
-# # Initialize the Africa's Talking client
-# africastalking.initialize(settings.AFRICASTALKING_USERNAME, settings.AFRICASTALKING_API_KEY)
-# sms = africastalking.SMS
-
-# def send_sms(phone_number, message):
-#     # Ensure the phone number is valid
-#     phone_regex = re.compile(r'^\+?1?\d{9,15}$')
-#     if not phone_regex.match(phone_number):
-#         raise ValueError(f"Invalid phone number format: {phone_number}")
-    
-#     try:
-#         response = sms.send(message, [phone_number], settings.AFRICASTALKING_SENDER_ID)
-#         print(response)
-#     except Exception as e:
-#         print(f"Failed to send SMS: {str(e)}")
 
 import africastalking
 from decouple import config  # To load .env variables
