d949715b375e10f2728476d84261b031b9995109
Harrison Hank <codinghank@gmail.com>
2024-09-25 13:48:07 +0300

    added a read me file

--- savannah/openId/tests.py
+++ savannah/openId/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Customer
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class CustomerModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.customer = Customer.objects.create(user=self.user, name='John Doe', code='CUST007', phone_number='+1234567890')
+
+    def test_customer_creation(self):
+        self.assertEqual(self.customer.name, 'John Doe')
+        self.assertEqual(self.customer.code, 'CUST007')
+        self.assertEqual(self.customer.phone_number, '+1234567890')
+
+    def test_customer_str(self):
+        self.assertEqual(str(self.customer), 'John Doe')
